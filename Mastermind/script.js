1d98dfc7413c5b45d1665344331729abc19ed540
Disha <singhdisha@gmail.com>
2023-08-02 10:21:31 +0530

    my commit

--- Mastermind/script.js
+++ Mastermind/script.js
@@ -3,6 +3,7 @@ const playButtons = document.querySelectorAll('.choice');
 const checkButton = document.querySelector('.subBtn');
 const restartButton = document.querySelector('.playAgain');
 const userGuesses = document.querySelectorAll('.user-guess');
+const hintPalette = document.querySelectorAll('.hint-palette');
 
 const colorObject={"red":1,"blue":2,"green":3,"yellow":4,"pink":5,"white":6,"purple":7,"orange":8};
 
@@ -38,30 +39,30 @@ const computerNumber = generateUniqueCode();
 const colorArray = mapNumberToColor(computerNumber);  //computer generated
 
 //displaying the random color
-
 function displayRandomColor(){
-const div = document.querySelectorAll('.ans');
-for(let i=0;i<div.length;i++){
-    const backgroundColor=colorArray[i];
-    div[i].style.backgroundColor=backgroundColor;
-    div[i].style.color=backgroundColor;
-}
+    const div = document.querySelectorAll('.ans');
+    for(let i=0;i<div.length;i++){
+        const backgroundColor=colorArray[i];
+        div[i].style.backgroundColor=backgroundColor;
+        div[i].style.color=backgroundColor;
+    }
 }
 
-//displayRandomColor();
 
 //restart game
 function restartGame(){
    window.location.reload();
-   saveUsernameToLocalStorage();
+   saveUserNameToLocalStorage();
 }
+restartButton.addEventListener('click',restartGame);
+
 
 //taking color from user-guess
 const selectedColors = [];
 
 // Function to get bulls and cows
 let result = [];
-
+let score;
 function getBullsAndCows(userGuess, computerNumber) {
   //creating the hint object
   const hint = {
@@ -96,14 +97,18 @@ function getBullsAndCows(userGuess, computerNumber) {
         count++;
     }
     if (count === 4) {
-        const savedUsername = localStorage.getItem('username');
+        saveScoreToLocalStorage();
+        // const savedUsername = localStorage.getItem('username');
+        score = 1;
         const username = savedUsername ? ` ${savedUsername}` : '';
-        window.alert(`Congratulations${username}!!! You won.`);
-        saveUsernameToLocalStorage();
+        // window.alert(`Congratulations${username}!!! You won.`);
         displayRandomColor();
         result = [];
         break;
+    }else{
+        score = 0;
     }
+    // saveScoreToLocalStorage(score);
   }
 }
 
@@ -150,7 +155,7 @@ function handleDrop(event) {
 
 //displaying bull cow in the one designated
 let num = 1;
-  function checkGuesses() {
+function checkGuesses() {
     let currentRoundID = `guess${num}`;
     let currentRoundDiv = document.getElementById(currentRoundID);
   
@@ -170,7 +175,7 @@ let num = 1;
     num++;
     if(num==11){
         window.alert(`Oh!! You failed to guess the color.Better luck next time!!The correct color combination was (${colorArray.join(', ')}).`);
-        displayPlayerName();
+        // displayPlayerName();
         displayRandomColor();
         //window.location.reload();
         //return;
@@ -204,55 +209,143 @@ function addHints(result, num) {
 
 }
 
-//add to loacl storage
-function saveUsernameToLocalStorage() {
-    const PlayerName = prompt('Please enter your name:');
-    if (PlayerName) {
-        localStorage.setItem('PlayerName', PlayerName);
-        alert(`Thanks "${PlayerName} for providing your Name"`);
-    } else {
-        alert('No username found');
-    }
+
+function showHintPalette(btn) {
+    const hintPalette = btn.parentElement.querySelector('.hint-palette');
+    hintPalette.style.display = 'block';
+    btn.style.display = 'none'; 
+    checkGuesses();
+}
+  
+// Event listener for the all Check button
+const Check1 = document.querySelector('.check1');
+Check1.addEventListener('click', function() {
+    showHintPalette(Check1);
+});
+
+const Check2 = document.querySelector('.check2');
+Check2.addEventListener('click', function() {
+    showHintPalette(Check2);
+});
+
+const Check3 = document.querySelector('.check3');
+Check3.addEventListener('click', function() {
+    showHintPalette(Check3);
+});
+
+const Check4 = document.querySelector('.check4');
+Check4.addEventListener('click', function() {
+    showHintPalette(Check4);
+});
+
+const Check5 = document.querySelector('.check5');
+Check5.addEventListener('click', function() {
+    showHintPalette(Check5);
+});
+
+const Check6 = document.querySelector('.check6');
+Check6.addEventListener('click', function() {
+    showHintPalette(Check6);
+});
+
+const Check7 = document.querySelector('.check7');
+Check7.addEventListener('click', function() {
+    showHintPalette(Check7);
+});
+
+const Check8 = document.querySelector('.check8');
+Check8.addEventListener('click', function() {
+    showHintPalette(Check8);
+});
+
+const Check9 = document.querySelector('.check9');
+Check9.addEventListener('click', function() {
+    showHintPalette(Check9);
+});
+
+const Check10 = document.querySelector('.check10');
+Check10.addEventListener('click', function() {
+    saveScoreToLocalStorage();
+    showHintPalette(Check10);
+});
+
+
+
+
+// PLAYING WITH LOCAL STORAGE
+
+var arr = [];
+if (localStorage.getItem("userData")) {
+  arr = JSON.parse(localStorage.getItem("userData"));
 }
 
-//add to dom
-function displayPlayerName() {
-    const savedPlayerName = localStorage.getItem('PlayerName');
-    const PlayerNameDisplayElement = document.getElementById('PlayerNameDisplay');
-    if (savedPlayerName) {
-        PlayerNameDisplayElement.textContent = `Player Name :- ${savedPlayerName}!`;
-    } else {
-        PlayerNameDisplayElement.textContent = `Welcome, Guest!`;
-    }
+// Add user name to local storage
+function saveUserNameToLocalStorage() {
+  const playerName = prompt('Please enter your name:');
+  if (playerName) {
+    const obj = { name: playerName };
+    arr.push(obj);
+    localStorage.setItem("userData", JSON.stringify(arr));
+    alert(`Thanks "${playerName}" for providing your Name`);
+  } else {
+    alert('No username found');
+  }
 }
 
-checkButton.addEventListener('click', checkGuesses);
-restartButton.addEventListener('click',restartGame);
+// Add score to local storage
+function saveScoreToLocalStorage() {
+  const scoreObj = { score: score };
+  arr.push(scoreObj);
+  localStorage.setItem("userData", JSON.stringify(arr));
+}
 
-// Saving data in local storage
-// function saveDataInLocalStorage() {
-//     localStorage.setItem("selectedColors", JSON.stringify(selectedColors));
-//     localStorage.setItem("result", JSON.stringify(result));
-// }
+// Add username and score to DOM
+var div = document.getElementById('PlayerNameDisplay');
+function displayDataToDom() {
+    let tableHTML = `
+        <h1>Leader Board</h1>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Score</th>
+            </tr>
+    `;
+    
+    for (let i = 0; i < arr.length; i += 2) {
+        const name = arr[i].name;
+        const score = (i + 1 < arr.length) ? arr[i + 1].score : 0;
+        
+        tableHTML += `
+            <tr>
+                <td id="name${i}">${name}</td>
+                <td id="score${i}">${score}</td>
+            </tr>
+        `;
+    }
 
-// Loading data from local storage
-// function loadDataFromLocalStorage() {
-//     const selectedColorsData = localStorage.getItem("selectedColors");
-//     const resultData = localStorage.getItem("result");
-
-//     if (selectedColorsData) {
-//         selectedColors = JSON.parse(selectedColorsData);
-//     }
-//     if (resultData) {
-//         result = JSON.parse(resultData);
-//     }
-// }
-
-// // Adding event listener to the "Check" button to save data before checking
-// checkButton.addEventListener('click',saveDataInLocalStorage);
+    tableHTML += `</table>`;
+    div.innerHTML = tableHTML;
+}
 
 
-// // Adding an event listener to the window to load data when the page loads
-// window.addEventListener('DOMContentLoaded', () => {
-//     loadDataFromLocalStorage();
-// });
+
+
+const checkScoreButton = document.querySelector('.checkScore');
+checkScoreButton.addEventListener('click', displayDataToDom);
+
+//display in another page
+const main = document.querySelector('.main');
+const board = document.getElementById('PlayerNameDisplay');
+const back = document.querySelector('.back');
+back.style.display = "none";
+
+checkScoreButton.addEventListener('click', function() {
+    main.style.display = "none";
+    board.style.display = "block";
+    back.style.display = "block";
+});
+
+back.addEventListener('click', function() {
+    window.location.reload();
+    back.style.display = "none";
+});
